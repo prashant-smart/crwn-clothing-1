bbe633e1a274e0809b099dc282e5b0398a189f10
Prashant Kumar Yadav <kartik224blue@gmail.com>
2021-05-15 01:13:14 +0700

    seprate our crwn app into client and a backend express for handle the stripe payment request

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,14 +1,34 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 const StripeCheckoutButton = ({ price }) => {
     const priceForStripe = price * 100;
     const publishableKey = 'pk_test_51ImwMSSF8F3ysptPAx81YJUE6KQEHAcnDJP8O7sswS99KsCqUJGWWA82fjElmbALK31oo7kt55efsY7b4gfTmUZG00A9WyujDC';
-  
+    
     const onToken = token => {
-    //   console.log(token); ------> we send this token in backend for details reated payment 
-      alert('Payment Succesful!');
+      // axios({
+      //   url: 'payment',
+      //   method: 'post',
+      //   data: {
+      //     amount: priceForStripe,
+      //     token: token
+      //   }
+      // })
+      //   .then(response => {
+      //     alert('succesful payment');
+      //   })
+      //   .catch(error => {
+      //     console.log('Payment Error: ', error);
+      //     alert(
+      //       'There was an issue with your payment! Please make sure you use the provided credit card.'
+      //     );
+      //   });
+      alert(
+        'succesful payment'
+             );
     };
+  
   
     return (
       <StripeCheckout
@@ -24,6 +44,5 @@ const StripeCheckoutButton = ({ price }) => {
         stripeKey={publishableKey}
       />
     );
-  };
-  
+  }
   export default StripeCheckoutButton;
